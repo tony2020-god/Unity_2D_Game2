10450088c8f1f6d6902df8718837920ba55c63b9
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2020-11-14 21:05:15 +0800

    程式

---
+++ 2DGame2/Assets/程式碼/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [Header("這角色速度"), Range(0, 1000)]
+    public float speed = 10.5f;
+    [Header("這角色跳躍"), Range(0, 3000)]
+    public int jump = 100;
+    public bool isGround = false;
+    private int score = 0 ;
+    public GameObject bullet;
+    public Transform point;
+    [Header("子彈速度"), Range(0, 5000)]
+    public int bulletspeed = 800;
+    public AudioClip music;
+    [Header("角色生命"), Range(0, 10)]
+    public int life = 3;
+    public Rigidbody2D rig;
+    public Animation ani;
+
+    public void Start()
+    {
+        print("Hello World");
+        Shoot(1, 500);
+        Shoot(2, 600);
+    }
+    public void Update()
+    {
+        
+    }
+    private void Drive(float speed)
+    {
+        print("開車中");
+        transform.Translate(speed,0, 0);
+    }
+    /// <summary>
+    /// 發射弓箭的功能
+    /// </summary>
+    /// <param name="count">弓箭數量</param>
+    /// <param name="speed">弓箭發射速度，預設值為300</param>
+    private void Shoot(int count ,int speed=300)
+    {
+        print("發射弓箭" + count);
+        print("弓箭速度" + speed);
+    }
+}
---
+++ 2DGame2/Assets/程式碼/car.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class car : MonoBehaviour
+{
+    [Header("這是汽車的cc數")]
+    public int cc = 200;
+    [Header("這是汽車的重量"), Range (500, 1000)]
+    public float weight = 1500.5f;
+    public string brand = "BNW";
+    [Header("是否有天窗")]
+    [Tooltip("勾選代表有天窗")]
+    public  bool window = true;
+
+
+    public Color red = Color.red;
+    public Color yellow = Color.yellow;
+    public Color mycolor = new Color(0.3f, 0, 0.9f);
+
+    public Vector2 pos0 = Vector2.zero;
+    public Vector2 pos1 = Vector2.one;
+    public Vector2 pos2 = new Vector2(7, 9);
+
+    public Vector3 posv3 = Vector3.one;
+    public Vector4 posv4 = Vector4.one;
+
+    public GameObject obj;
+    public Transform tra;
+    public SpriteRenderer spr;
+
+}
