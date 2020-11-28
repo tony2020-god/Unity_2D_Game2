416485d7c8a203b8834dee40fe68c7abb8395af6
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2020-11-28 21:03:05 +0800

    人物和攝影師移動

---
+++ 2DGame2/Assets/程式碼/CameraControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    [Header("目標")]
+    public Transform target;
+    [Header("追蹤速度"),Range(0,100)]
+    public float speed = 1.5f;
+    [Header("攝影機下方與上方的限制")]
+    public Vector2 limit = new Vector2(0, 3.5f);
+
+    private void Track()
+    {
+        Vector3 posA = transform.position;    //取得攝像機座標
+        Vector3 posB = target.position;       //取得目標座標
+        posB.z = -10;                         //固定z軸
+        posB.y = Mathf.Clamp(posB.y, limit.x, limit.y); //將y軸夾在限制範圍內
+        posB.x = Mathf.Clamp(posB.x, 0, 160); //將x軸夾在限制範圍內
+        //一禎的時間 Time.deltaTime
+        posA = Vector3.Lerp(posA, posB, speed * Time.deltaTime);//插值
+
+        transform.position = posA;            //攝影機座標 = A點
+    }
+    private void LateUpdate()
+    {
+        Track();
+    }
+}
--- 2DGame2/Assets/程式碼/Player.cs
+++ 2DGame2/Assets/程式碼/Player.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
@@ -18,17 +19,30 @@ public class Player : MonoBehaviour
     [Header("角色生命"), Range(0, 10)]
     public int life = 3;
     public Rigidbody2D rig;
-    public Animation ani;
+    public Animator ani;
+    public AudioSource aud;
+    public AudioClip SoundFire;
+    public Vector2 offset;
+    public float radius = 0.3f;
 
+    public void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
+        aud = GetComponent<AudioSource>();
+    }
     public void Start()
     {
         print("Hello World");
         Shoot(1, 500);
         Shoot(2, 600);
+       
     }
     public void Update()
     {
         Move();
+        Fire();
+        Jump();
     }
     private void Drive(float speed)
     {
@@ -55,20 +69,86 @@ public class Player : MonoBehaviour
         //剛體 的 加速度 = 新 二維向量(水平浮點數 * 速度, 剛體的加速度Y)
         rig.velocity = new Vector2(h * speed, rig.velocity.y);
         print("水平數值" + h);
+        //動畫的設定布林值(參數名稱，水平 不等於零時勾選)
+        ani.SetBool("跑步開關", h != 0);
+        //keycode 列單(下拉式選單) 所有輸入的選項:滑鼠、鍵盤、搖桿
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            //此物件的變形元件
+            //eulerAngles 歐拉角度
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
     }
     /// <summary>
     /// 攻擊
     /// </summary>
     private void Fire()
     {
-
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            aud.PlayOneShot(SoundFire);
+           GameObject temp = Instantiate(bullet, point.position,point.rotation);
+            //讓子彈飛
+            //上 綠 transform.up
+            //右 紅 transform.right
+            //左 藍 transform.forward
+            temp.GetComponent<Rigidbody2D>().AddForce(transform.right* bulletspeed + transform.up*100);
+        }
     }
     /// <summary>
     /// 跳躍
     /// </summary>
     private void Jump()
     {
-
+        //如果角色在地板上面 並且按下空白鍵 才能跳躍
+        if (isGround && Input.GetKeyDown(KeyCode.Space))
+        {
+            rig.AddForce(transform.up * jump);
+        }
+        //如果 物理 圖形 碰到圖層 8的地板物件
+        else if (Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y) + offset, radius,1 << 8))
+        {
+            isGround = true;
+        }
+        //沒有碰到地板物件
+        else
+        {
+            isGround = false;   //不再地面上了
+        }
     }
-
+    private void Dead(string obj)
+    {
+        if (obj == "死亡界線")
+        {
+            enabled = false;
+            ani.SetBool("死亡觸發", true);
+            //延遲呼叫("方法名稱",延遲時間)
+            Invoke("Replay", 2.5f);
+        }
+        
+    }
+    private void Replay()
+    {
+        SceneManager.LoadScene("關卡1");
+    }
+    /// <summary>
+    /// ODE碰撞時執行一次的事件
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Dead(collision.gameObject.name);
+    }
+    private void OnDrawGizmos()
+    {
+        //圖示 顏色
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        //圖示 繪製球體(中心點、半徑)
+        Gizmos.DrawSphere(new Vector2(transform.position.x, transform.position.y) + offset, radius);
+    }
 }
